3f110bcdd31e928828216dc7b03b637d8ca29e94
tschuser <tschuser@students.zhaw,ch>
2019-07-05 15:21:09 +0200

    Änderungen Seraina

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ProjectEndpoint.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/ProjectEndpoint.java
@@ -3,10 +3,13 @@ package ch.zhaw.sml.iwi.meng.leantodo.boundary;
 import java.security.Principal;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,9 +47,9 @@ public class ProjectEndpoint {
     }
 
     
-    @RequestMapping(path = "/api/project/{id}", method = RequestMethod.POST)
+    @RequestMapping(path = "/api/project/", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
-    public void addToDo(@RequestParam(name="id") Long projectId, @RequestBody ToDo toDo,  Principal principal) {
+    public void addToDo(@RequestParam("id") Long projectId, @RequestBody ToDo toDo,  Principal principal) {
         projectController.addToDo(projectId, toDo, principal.getName());
     }
 }
